5791c946a852a3ef6200949433cf86dd733d6dbf
mantaslv <mantasvolkas@gmail.com>
2023-03-16 17:57:36 +0000

    Shows list of workouts on homepage

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from './pages/home';
 import Navbar from './components/navbar';
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className='pages'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className='pages'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
