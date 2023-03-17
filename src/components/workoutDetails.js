553bb815961e295b035fe7a25ffc1d3bf90b172b
mantaslv <mantasvolkas@gmail.com>
2023-03-17 11:58:07 +0000

    Can delete workouts

--- src/components/workoutDetails.js
+++ src/components/workoutDetails.js
@@ -1,4 +1,19 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+    const  { dispatch } = useWorkoutsContext();
+    
+    const handleClick = async () => {
+        const res = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        });
+        const json = await res.json();
+
+        if (res.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        };
+    };
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
@@ -6,6 +21,7 @@ const WorkoutDetails = ({ workout }) => {
             <p><strong>Sets: </strong>{workout.sets}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     );
 };
