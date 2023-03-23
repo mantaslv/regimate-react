9102f9ec0caa733b7e40b6b4e927cc7903d1ece0
mantaslv <mantasvolkas@gmail.com>
2023-03-23 16:46:20 +0000

    Modified api file to inlcude deployed backend url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const apiBaseUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return process.env.REACT_APP_API_URL;
+    return "https://regimate.onrender.com/";
   } else {
     return "";
   };
