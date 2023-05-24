bb12b86939f331c156714c374b7a88d2ed32646e
mantaslv <mantasvolkas@gmail.com>
2023-05-24 16:16:28 +0100

    Sidenav shows logo

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,10 +1,30 @@
-import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { AddCircle, Article, Home, Logout } from '@mui/icons-material';
+import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { AddCircle, Article, FitnessCenter, Home, Logout } from '@mui/icons-material';
 
 const SideNav = () => {
     return (
         <Drawer variant="permanent">
-            <Box sx={{ width: 240, backgroundColor: 'primary' }}>
+            <Box sx={{ width: 240 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', p: 2 }}>
+                    <FitnessCenter sx={{ display: 'flex', mr: 1 }} />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            flexGrow: 1
+                        }}
+                    >
+                        REGIMATE
+                    </Typography>
+                </Box>
+                
                 <List>
                     <ListItemButton>
                         <ListItemIcon>
