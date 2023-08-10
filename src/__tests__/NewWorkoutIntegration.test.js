a6b0657a15db61835b3018a6d55f4cc560742607
mantaslv <mantasvolkas@gmail.com>
2023-08-10 18:33:49 +0100

    Added exerciseNameInput to test

--- src/__tests__/NewWorkoutIntegration.test.js
+++ src/__tests__/NewWorkoutIntegration.test.js
@@ -17,7 +17,7 @@ test('Integration Test: Entering set values updates context states', () => {
             <ExerciseContextProvider>
                 <SetContextProvider>
                     <WorkoutComponent 
-                        exerciseList={['Squat']} 
+                        exerciseList={['Squats']} 
                         theme={{ palette: { primary: { main: '#000000' }}}}
                         onContextStateChange={handleContextStateChange}
                     />
@@ -26,11 +26,16 @@ test('Integration Test: Entering set values updates context states', () => {
         </WorkoutContextProvider>
     );
 
+    const exerciseNameInput = getByLabelText('Exercise Name');
     const weightInput = getByLabelText('Weight (kg)');
     const repsInput = getByLabelText('Reps');
 
+    fireEvent.change(exerciseNameInput, { target: { value: 'Squats' } });
     fireEvent.change(weightInput, { target: { value: '50' } });
     fireEvent.change(repsInput, { target: { value: '10' } });
 
+    console.log(capturedContextState)
+
+    expect(capturedContextState[0]).toEqual(expect.objectContaining({ exerciseName: 'Squats' }));
     expect(capturedContextState[0].sets[0]).toEqual(expect.objectContaining({ weight: '50', reps: '10' }));
 });
