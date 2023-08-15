680fa552e9797ede4dd40fcead4f24d560c1a537
mantaslv <mantasvolkas@gmail.com>
2023-08-15 17:01:24 +0100

    Exercise name text field added to box

--- src/pages/NewProgramme.js
+++ src/pages/NewProgramme.js
@@ -1,4 +1,4 @@
-import { Box, Grid, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { Autocomplete, Box, Grid, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import { useState } from "react";
 
 const NewProgrammePage = () => {
@@ -25,11 +25,11 @@ const NewProgrammePage = () => {
             </ToggleButtonGroup>
             <Grid container marginY={1} spacing={1} justifyContent="center" alignItems="center" textAlign="center">
                 {Array.from({ length: split + 3 }, (_, i) => 
-                    <Grid item md={2}>
-                        
+                    <Grid item md={4}>
                         <Box
                             sx={{
-                                display: 'inline-flex',
+                                display: 'flex',
+                                flexDirection: 'column', 
                                 alignItems: 'center',
                                 justifyContent: 'center',
                                 borderRadius: '16px',
@@ -42,6 +42,15 @@ const NewProgrammePage = () => {
                             }}
                         >
                             <Typography>Day {i + 1}</Typography>
+                            <Autocomplete
+                                disablePortal
+                                freeSolo
+                                options={['squats']}
+                                name="exerciseName"
+                                onInputChange={() => {}}
+                                sx={{ width: '100%', mt: 1 }}
+                                renderInput={(params) => <TextField {...params} label="Exercise Name" data-testid="exercise-input"/>}
+                            />
                         </Box>
                     </Grid>
                 )}
