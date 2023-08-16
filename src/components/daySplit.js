e75223dcec2c07d770c3ba811dd0a0b255b60923
mantaslv <mantasvolkas@gmail.com>
2023-08-16 15:07:51 +0100

    Day split shows chosen exercises

---
+++ src/components/daySplit.js
@@ -0,0 +1,89 @@
+import { Box, Button, Card, CardHeader, Dialog, DialogContent, DialogTitle, List, ListItemButton, ListItemText, TextField, Typography } from "@mui/material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useState } from "react";
+
+const exercises = ["Front Squats", "Back Squats", "Dips", "Push Ups", "Pull Ups", "Plank"];
+
+const DaySplit = ({ index }) => {
+    const [openExerciseSelector, setOpenExerciseSelector] = useState(false);
+    const [chosenExercises, setChosenExercises] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredExercises = exercises.filter(exercise =>
+        exercise.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleExerciseSelection = (exercise) => {
+        setChosenExercises(prevChosenExercises => [...prevChosenExercises, exercise]);
+        setOpenExerciseSelector(false);
+    };
+
+    return(
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column', 
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: '16px',
+                border: '3px solid',
+                borderColor: 'grey.400',
+                backgroundColor: 'white',
+                color: 'black',
+                padding: '8px 16px',
+                width: '80%'
+            }}
+        >
+            <Typography color="grey.700">Day {index + 1}</Typography>
+            {chosenExercises && chosenExercises.map(e => 
+                <Card 
+                    variant="outlined"
+                    sx={{
+                        margin: 1,
+                        borderRadius: '16px',
+                        border: '2px solid',
+                        borderColor: 'grey.400',
+                        width: '100%'
+                    }}
+                >
+                    <CardHeader title={e} color="primary"/>
+                </Card>
+            )}
+            <Button
+                onClick={() => setOpenExerciseSelector(true)}
+                sx={{
+                    margin: 1,
+                    borderRadius: '16px',
+                    border: '3px dashed',
+                    borderColor: 'grey.400',
+                    width: '100%'
+                }}
+            >
+                <AddCircleOutlineIcon sx={{ color: 'grey.400', fontSize: 30 }} />
+            </Button>
+            <Dialog 
+                open={openExerciseSelector} 
+                onClose={() => setOpenExerciseSelector(false)}
+                slotProps={{ backdrop: { sx: { backgroundColor: 'rgba(0, 0, 0, 0.1)' } } }}
+            >
+                <DialogTitle>Select Exercise</DialogTitle>
+                <DialogContent>
+                    <TextField label="Search" value={searchTerm} onChange={handleSearchChange} sx={{ mt: 1 }}/>
+                    <List>
+                        {filteredExercises.map(e => 
+                            <ListItemButton key={e} onClick={() => handleExerciseSelection(e)}>
+                                <ListItemText primary={e}/>
+                            </ListItemButton>
+                        )}
+                    </List>
+                </DialogContent>
+            </Dialog>
+        </Box>
+    )
+}
+
+export default DaySplit;
--- src/pages/NewProgramme.js
+++ src/pages/NewProgramme.js
@@ -1,25 +1,12 @@
-import { Box, Button, Dialog, DialogContent, DialogTitle, Grid, List, ListItemButton, ListItemText, TextField, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { Box, Grid, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
 import { useState } from "react";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-
-const exercises = ["Front Squats", "Back Squats", "Dips", "Push Ups", "Pull Ups", "Plank"];
+import DaySplit from "../components/daySplit";
 
 const NewProgrammePage = () => {
     const [split, setSplit] = useState(0);
-    const [openExerciseSelector, setOpenExerciseSelector] = useState(false);
-    const [searchTerm, setSearchTerm] = useState("");
-    const [day1, setDay1] = useState([])
     
     const handleSplitToggle = (_, chosenSplit) => {
         setSplit(chosenSplit)
-    };
-
-    const filteredExercises = exercises.filter(exercise =>
-        exercise.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    const handleSearchChange = (event) => {
-        setSearchTerm(event.target.value);
     };
 
     return (
@@ -32,65 +19,13 @@ const NewProgrammePage = () => {
                 sx={{ display: 'flex', justifyContent: 'center', margin: 2 }}
             >
                 {Array.from({ length: 4 }, (_, i) => 
-                    <ToggleButton key={i} value={i}>
-                        {i+3}-Day Split
-                    </ToggleButton>
+                    <ToggleButton key={i} value={i}>{i+3}-Day Split</ToggleButton>
                 )}
             </ToggleButtonGroup>
             <Grid container marginY={1} spacing={1} justifyContent="center" alignItems="center" textAlign="center">
                 {Array.from({ length: split + 3 }, (_, i) => 
                     <Grid item md={4}>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column', 
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                borderRadius: '16px',
-                                border: '3px solid',
-                                borderColor: 'grey.400',
-                                backgroundColor: 'white',
-                                color: 'black',
-                                padding: '8px 16px',
-                                width: '80%'
-                            }}
-                        >
-                            <Typography color="grey.700">Day {i + 1}</Typography>
-                            <Button
-                                onClick={() => setOpenExerciseSelector(true)}
-                                sx={{
-                                    margin: 1,
-                                    borderRadius: '16px',
-                                    border: '3px dashed',
-                                    borderColor: 'grey.400',
-                                    width: '100%'
-                                }}
-                            >
-                                <AddCircleOutlineIcon
-                                    sx={{
-                                        color: 'grey.400',
-                                        fontSize: 30
-                                    }}
-                                />
-                            </Button>
-                            <Dialog 
-                                open={openExerciseSelector} 
-                                onClose={() => setOpenExerciseSelector(false)}
-                                slotProps={{ backdrop: { sx: { backgroundColor: 'rgba(0, 0, 0, 0.1)' } } }}
-                            >
-                                <DialogTitle>Select Exercise</DialogTitle>
-                                <DialogContent>
-                                    <TextField label="Search" value={searchTerm} onChange={handleSearchChange} sx={{ mt: 1 }}/>
-                                    <List>
-                                        {filteredExercises.map(e => 
-                                            <ListItemButton>
-                                                <ListItemText primary={e}/>
-                                            </ListItemButton>
-                                        )}
-                                    </List>
-                                </DialogContent>
-                            </Dialog>
-                        </Box>
+                        <DaySplit index={i}/>
                     </Grid>
                 )}
             </Grid>
