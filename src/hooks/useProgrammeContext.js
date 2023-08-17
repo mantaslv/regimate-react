5040a44f66d8c52d0b8a9c5a42eac983582ef55c
mantaslv <mantasvolkas@gmail.com>
2023-08-17 11:41:52 +0100

    Hook created for useProgrammeContext

---
+++ src/hooks/useProgrammeContext.js
@@ -0,0 +1,12 @@
+import { ProgrammeContext } from "../context/programmeContext";
+import { useContext } from "react";
+
+export const useProgrammeContext = () => {
+    const context = useContext(ProgrammeContext);
+
+    if (!context) {
+        throw Error('useProgrammeContext must be used inside a ProgrammeContextProvider');
+    };
+
+    return context;
+};
