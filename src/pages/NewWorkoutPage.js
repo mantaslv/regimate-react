0ea6ea4a34b7feb0d5538027a93f0f9c331cd597
mantaslv <mantasvolkas@gmail.com>
2023-08-21 14:38:51 +0100

    Removed obsolete code

--- src/pages/NewWorkoutPage.js
+++ src/pages/NewWorkoutPage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
 
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
