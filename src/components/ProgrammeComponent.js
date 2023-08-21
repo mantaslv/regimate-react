12de39ba65bd72a130afa41c2acfb0f761c39b20
mantaslv <mantasvolkas@gmail.com>
2023-08-21 10:55:12 +0100

    Moved programme page into component

---
+++ src/components/ProgrammeComponent.js
@@ -0,0 +1,58 @@
+import { Box, Button, Grid, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import { useState } from "react";
+import { useProgrammeContext } from "../hooks/useProgrammeContext";
+import TerminalIcon from '@mui/icons-material/Terminal';
+import { WorkoutContextProvider } from "../context/workoutContext";
+import WorkoutComponent from "../components/WorkoutComponent";
+
+const ProgrammeComponent = () => {
+    const { state } = useProgrammeContext();
+    const [split, setSplit] = useState(0);
+    
+    const handleSplitToggle = (_, chosenSplit) => {
+        setSplit(chosenSplit)
+    };
+
+    return (
+        <Box sx={{ mt: 10 }}>
+            <Typography variant="h5" color="primary" sx={{ textAlign: 'center', mb: 1 }}>
+                New Programme
+            </Typography>
+            <ToggleButtonGroup
+                value={split} 
+                exclusive 
+                onChange={handleSplitToggle}
+                sx={{ display: 'flex', justifyContent: 'center', margin: 2 }}
+            >
+                {Array.from({ length: 4 }, (_, i) => 
+                    <ToggleButton key={i} value={i}>
+                        {i+3}-Day Split
+                    </ToggleButton>
+                )}
+            </ToggleButtonGroup>
+            <Grid 
+                container 
+                marginY={1} 
+                spacing={3} 
+                justifyContent="center" 
+                alignItems="top" 
+                textAlign="center"
+            >
+                {state.workouts.slice(0, split + 3).map((_, i) =>
+                    <Grid item key={i} md={split === 0 ? 4 : split === 1 ? 3 : 2}>
+                        <WorkoutContextProvider>
+                            <WorkoutComponent exerciseList={["Squat"]} programme={true} index={i}/>
+                        </WorkoutContextProvider>
+                    </Grid>
+                )}
+            </Grid>
+            <Button
+                variant="contained" 
+                onClick={() => console.log(state)}
+                title="Click to console log this workout"
+            ><TerminalIcon/></Button>
+        </Box>
+    );
+};
+
+export default ProgrammeComponent;
