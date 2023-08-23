321988d0ecc1c3aa09be529e33acba8418f67a9c
mantaslv <mantasvolkas@gmail.com>
2023-08-23 10:20:27 +0100

    Programmes context created

---
+++ src/context/programmesContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react';
+
+export const ProgrammesContext = createContext();
+
+export const programmesReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_PROGRAMMES':
+            return { programmes: action.payload };
+        case 'CREATE_PROGRAMME':
+            return { programmes: [action.payload, ...state.programmes] };
+        case 'DELETE_PROGRAMME':
+            return { programmes: state.programmes.filter((w) => w._id !== action.payload._id) };
+        default: 
+            return state;
+    }
+}
+
+export const ProgrammesContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(programmesReducer, {
+        programmes: null
+    });
+
+    return (
+        <ProgrammesContext.Provider value={{ state, dispatch }}>
+            { children }
+        </ProgrammesContext.Provider>
+    );
+};
---
+++ src/hooks/useProgrammesContext.js
@@ -0,0 +1,12 @@
+import { ProgrammesContext } from "../context/programmesContext";
+import { useContext } from "react";
+
+export const useProgrammesContext = () => {
+    const context = useContext(ProgrammesContext);
+
+    if (!context) {
+        throw Error('useProgrammesContext must be used inside a ProgrammesContextProvider');
+    };
+
+    return context;
+};
