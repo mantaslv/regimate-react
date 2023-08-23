4721f6ec082d2b778ec26354ad3e0dfb71a1e58c
mantaslv <mantasvolkas@gmail.com>
2023-08-23 14:38:18 +0100

    Exercise directory added from github repo wrkout/exercises.json

--- src/components/ExerciseSelector.js
+++ src/components/ExerciseSelector.js
@@ -1,13 +1,44 @@
 import { Dialog, DialogContent, DialogTitle, List, ListItemButton, ListItemText, TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 
-const exercises = ["Front Squats", "Back Squats", "Dips", "Push Ups", "Pull Ups", "Plank"];
+// const exercises = ["Front Squats", "Back Squats", "Dips", "Push Ups", "Pull Ups", "Plank"];
 
 const ExerciseSelector = ({ openExerciseSelector, setOpenExerciseSelector, handleExerciseSelection, handleDeleteExercise, exerciseNotYetChosen }) => {
+    const { user } = useAuthContext();
     const [searchTerm, setSearchTerm] = useState("");
+    const [exercises, setExercises] = useState([]);
+
+    useEffect(() => {
+        const fetchExercises = async () => {
+            try {
+                const res = await fetch(process.env.REACT_APP_API_URL + '/api/exercise-directory', {
+                    mode: 'cors',
+                    headers: {
+                        'Authorization': `Bearer ${user.token}`,
+                        credentials: 'include'
+                    }
+                });
+                const json = await res.json();
+
+                console.log(json);
+
+                if (res.ok) {
+                    setExercises(json);
+                };
+
+            } catch(error) {
+                console.error("Error fetching exercises:", error);
+            };
+        };
+
+        if (user) {
+            fetchExercises();
+        };
+    }, [user]);
 
     const filteredExercises = exercises.filter(exercise =>
-        exercise.toLowerCase().includes(searchTerm.toLowerCase())
+        exercise.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     const handleSearchChange = (event) => {
@@ -30,9 +61,9 @@ const ExerciseSelector = ({ openExerciseSelector, setOpenExerciseSelector, handl
             <DialogContent>
                 <TextField label="Search" value={searchTerm} onChange={handleSearchChange} sx={{ mt: 1 }}/>
                 <List>
-                    {filteredExercises.map(e => 
-                        <ListItemButton key={e} onClick={() => handleExerciseSelection(e)}>
-                            <ListItemText primary={e}/>
+                    {filteredExercises.map((exercise, i) => 
+                        <ListItemButton key={i} onClick={() => handleExerciseSelection(exercise.name)}>
+                            <ListItemText primary={exercise.name}/>
                         </ListItemButton>
                     )}
                 </List>
