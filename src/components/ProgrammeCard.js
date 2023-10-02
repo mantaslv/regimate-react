e9907a26035dc6121833a88a2b6b9c6a4873a3e0
mantaslv <mantasvolkas@gmail.com>
2023-10-02 15:19:19 +0100

    Imported play circle icon to programme card

--- src/components/ProgrammeCard.js
+++ src/components/ProgrammeCard.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { Alert, Button, Card, CardContent, CardHeader, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import EditIcon from '@mui/icons-material/Edit';
-import { useState } from "react";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useProgrammesContext } from "../hooks/useProgrammesContext";
-import { useNavigate } from "react-router-dom";
 
 const ProgrammeCard = ({ programme, sx }) => {
     const { user } = useAuthContext();
