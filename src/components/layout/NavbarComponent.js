c8b0d4cff8423b6ac2e0c718e86da9bdf2c74bc8
mantaslv <mantasvolkas@gmail.com>
2023-10-18 14:08:21 +0100

    Created logo comp

--- src/components/layout/NavbarComponent.js
+++ src/components/layout/NavbarComponent.js
@@ -1,8 +1,8 @@
 import { useLogout } from '../../hooks/useLogout';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import { AppBar, Box, Button, Stack, Typography } from '@mui/material';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import { AppBar, Box, Button, Stack } from '@mui/material';
 import { items } from '../../options/navOptions';
+import { Logo } from '../Logo';
 
 const Navbar = () => {
     const { logout } = useLogout();
@@ -17,30 +17,22 @@ const Navbar = () => {
             <AppBar position="fixed">
                 <Box sx={{ display: 'flex', mx: 2 }}>
                     <Box sx={{ p: 1, display: 'flex', flexGrow: 1 }}>
-                        <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
-                            <FitnessCenterIcon  sx={{ display: 'flex', mr: 1 }}/>
-                            <Typography 
-                                variant="h5" 
-                                sx={{ letterSpacing: '.3rem', fontFamily: 'Roboto Mono' }}
-                            >
-                                REGIMATE
-                            </Typography>
-                        </Box>
+                        <Logo/>
                     </Box>
                     <Stack direction='row' gap={0.5} >
                         {user && items.map(item => (
-                            <Button 
-                                key={item.title} 
-                                href={item.path} 
+                            <Button
+                                key={item.title}
+                                href={item.path}
                                 sx={{ color: 'white', my: 0 }}
                             >
                                 {item.title}
                             </Button>
                         ))}
                         {user && (
-                            <Button 
-                                key="logout" 
-                                onClick={handleClick} 
+                            <Button
+                                key="logout"
+                                onClick={handleClick}
                                 sx={{ color: 'white', my: 0 }}
                             >
                                 Log out
