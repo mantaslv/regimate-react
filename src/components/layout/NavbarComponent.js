e399ea44a7b4b46d24c56f7d4f1b81cab1f2b3dd
mantaslv <mantasvolkas@gmail.com>
2023-10-19 13:21:51 +0100

    Added little white arrow to navbar to show which path is chosen

--- src/components/layout/NavbarComponent.js
+++ src/components/layout/NavbarComponent.js
@@ -3,10 +3,12 @@ import { useAuthContext } from '../../hooks/useAuthContext';
 import { AppBar, Box, Button, Stack } from '@mui/material';
 import { items } from '../../options/navOptions';
 import { Logo } from '../Logo';
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
     const { logout } = useLogout();
     const { user } = useAuthContext();
+    const location = useLocation();
 
     const handleClick = () => {
         logout();
@@ -20,15 +22,6 @@ const Navbar = () => {
                         <Logo/>
                     </Box>
                     <Stack direction='row' gap={0.5} >
-                        {/* {user && items.map(item => (
-                            <Button
-                                key={item.title}
-                                href={item.path}
-                                sx={{ color: 'white', my: 0 }}
-                            >
-                                {item.title}
-                            </Button>
-                        ))} */}
                         {items
                             .filter(item => user ? item.authRequired !== null : !item.authRequired)
                             .map(item => (
@@ -38,6 +31,23 @@ const Navbar = () => {
                                     sx={{ color: 'white', my: 0 }}
                                 >
                                     {item.title}
+                                    {location.pathname === item.path && (
+                                        <div
+                                            className="triangle" // Class for the triangle
+                                            style={{
+                                                width: 0,
+                                                height: 0,
+                                                borderBottom: '6px solid white',
+                                                borderLeft: '6px solid transparent',
+                                                borderRight: '6px solid transparent',
+                                                position: 'absolute',
+                                                bottom: 0,
+                                                left: '50%',
+                                                transform: 'translateX(-50%)', // Center horizontally
+                                            }}
+                                        />
+                                    )}
+                                    
                                 </Button>
                             ))
                         }
