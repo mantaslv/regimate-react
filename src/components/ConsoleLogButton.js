7a626d9ade9cb2cfca2c5d4bd55b3a624237222f
mantaslv <mantasvolkas@gmail.com>
2023-10-19 12:45:20 +0100

    Console log button conditional on dev

--- src/components/ConsoleLogButton.js
+++ src/components/ConsoleLogButton.js
@@ -2,15 +2,19 @@ import { Button } from "@mui/material";
 import TerminalIcon from '@mui/icons-material/Terminal';
 
 const ConsoleLogButton = ({ print, info }) => {
-    return (
-        <Button
-            variant="contained" 
-            onClick={() => console.log(print)}
-            title={`Click to console log this ${info}`}
-        >
-            <TerminalIcon/>
-        </Button>
-    );
+    if (process.env.NODE_ENV === 'development') {
+        return
+    } else {
+        return (
+            <Button
+                variant="contained" 
+                onClick={() => console.log(print)}
+                title={`Click to console log this ${info}`}
+            >
+                <TerminalIcon/>
+            </Button>
+        );
+    };
 };
 
 export default ConsoleLogButton;
