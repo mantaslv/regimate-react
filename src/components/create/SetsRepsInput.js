632fa537706c61698c2e2735dde07993af33b319
mantaslv <mantasvolkas@gmail.com>
2023-10-23 13:37:21 +0100

    Refactored sets and reps input

--- src/components/create/SetsRepsInput.js
+++ src/components/create/SetsRepsInput.js
@@ -1,22 +1,61 @@
 import { Input, Typography } from "@mui/material"
 import { useEffect, useState } from "react";
 
+const NamedInput = ({label, value, setVariable}) => {
+    const [variableValue, setVariableValue] = useState(value);
+
+    const handleInputChange = (event) => {
+        const inputValue = event.target.value;
+        const filteredValue = inputValue.replace(/[^0-9]/g, '').substring(0, 2);
+        event.target.value = filteredValue;
+        setVariableValue(filteredValue);
+        setVariable(filteredValue);
+    };
+
+    useEffect(() => {
+        setVariableValue(value);
+    }, [value])
+
+    return (
+        <>
+            <Typography 
+                variant="h6" 
+                fontSize={15} 
+                fontWeight={500} 
+                textTransform="none" 
+                sx={{ color: 'white', mr: -0.5 }}
+            >
+                {label}
+            </Typography>
+            <Input
+                disableUnderline
+                value={variableValue}
+                onChange={handleInputChange}
+                sx={{
+                    m: 1,
+                    width: 35,
+                    fontSize: 15,
+                    color: 'white',
+                    borderRadius: '10px',
+                    backgroundColor: '#4338CA', 
+                    '& input': { textAlign: 'center' },
+                    '&:hover': { backgroundColor: '#312E81' },
+                }}
+            />
+        </>
+    )
+}
+
 const SetsRepsInput = ({ handleSetsRepsChange, initialExerciseData }) => {
     const[sets, setSets] = useState(1);
     const[reps, setReps] = useState(1);
 
-    const handleSetsInputChange = (event) => {
-        const inputValue = event.target.value;
-        const filteredValue = inputValue.replace(/[^0-9]/g, '').substring(0, 2);
-        event.target.value = filteredValue;
-        setSets(filteredValue);
+    const handleSetsChange = (sets) => {
+        setSets(sets);
     };
 
-    const handleRepsInputChange = (event) => {
-        const inputValue = event.target.value;
-        const filteredValue = inputValue.replace(/[^0-9]/g, '').substring(0, 2);
-        event.target.value = filteredValue;
-        setReps(filteredValue);
+    const handleRepsChange = (reps) => {
+        setReps(reps);
     };
 
     useEffect(() => {
@@ -28,58 +67,12 @@ const SetsRepsInput = ({ handleSetsRepsChange, initialExerciseData }) => {
             setSets(initialExerciseData.sets.length);
             setReps(initialExerciseData.sets[0].reps)
         };
-    });
+    }, []);
 
     return (
         <>
-            <Typography 
-                variant="h6" 
-                fontSize={15} 
-                fontWeight={500} 
-                textTransform="none" 
-                sx={{ color: 'white', mr: -0.5 }}
-            >
-                Sets
-            </Typography>
-            <Input
-                disableUnderline
-                value={sets}
-                onChange={handleSetsInputChange}
-                sx={{
-                    m: 1,
-                    width: 35,
-                    fontSize: 15,
-                    color: 'white',
-                    borderRadius: '10px',
-                    backgroundColor: '#007368', 
-                    '& input': { textAlign: 'center' },
-                    '&:hover': { backgroundColor: '#008579' },
-                }}
-            />
-            <Typography 
-                variant="h6" 
-                fontSize={15} 
-                fontWeight={500} 
-                textTransform="none" 
-                sx={{ color: 'white', mr: -0.5 }}
-            >
-                Reps
-            </Typography>
-            <Input
-                disableUnderline
-                value={reps}
-                onChange={handleRepsInputChange}
-                sx={{
-                    m: 1,
-                    width: 35,
-                    fontSize: 15,
-                    color: 'white', 
-                    borderRadius: '10px',
-                    backgroundColor: '#007368', 
-                    '& input': { textAlign: 'center' },
-                    '&:hover': { backgroundColor: '#008579' },
-                }}
-            />
+            <NamedInput label='Sets' setVariable={handleSetsChange} value={sets}/>
+            <NamedInput label='Reps' setVariable={handleRepsChange} value={reps}/>
         </>
     );
 };
