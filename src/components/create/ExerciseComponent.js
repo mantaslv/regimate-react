bc1f3bf6afe0157751f1c4827797cc379749424e
mantaslv <mantasvolkas@gmail.com>
2023-10-23 15:45:39 +0100

    Moved workout exercise card into own comp

--- src/components/create/ExerciseComponent.js
+++ src/components/create/ExerciseComponent.js
@@ -1,22 +1,16 @@
 import { useEffect, useState } from "react";
-import { Box, Button, Card, CardContent, CardHeader, Grid, IconButton, Typography } from "@mui/material";
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-
-import SetComponent from "./SetComponent";
 import ExerciseSelector from "./ExerciseSelector";
 import { useExerciseContext } from "../../hooks/useExerciseContext";
-import { SetContextProvider } from "../../context/setContext";
 import { useWorkoutContext } from "../../hooks/useWorkoutContext";
-import ConsoleLogButton from "../ConsoleLogButton";
-import SetsRepsInput from "./SetsRepsInput";
 import { ProgrammeExerciseCard } from "./ProgrammeExerciseCard";
+import { WorkoutExerciseCard } from "./WorkoutExerciseCard";
 
-const Exercise = ({ 
+const Exercise = ({
+    exerciseList, 
     onExerciseChange, 
     onExerciseDelete, 
-    exerciseList, 
-    programme=false,
-    initialExerciseData
+    initialExerciseData,
+    programme=false
 }) => {
     const { state: workoutState } = useWorkoutContext();
     const { dispatch, state } = useExerciseContext();
@@ -48,11 +42,9 @@ const Exercise = ({
         onExerciseDelete();
     };
 
-    const handleExerciseNameChange = (newName) => {
-        setExerciseName(newName);
-        dispatch({ type: "UPDATE_EXERCISE_NAME", payload: newName });
-        setOpenExerciseSelector(false)
-    };
+    const handleOpenSelector = () => {
+        setOpenExerciseSelector(true)
+    };    
 
     const addSet = () => {
         dispatch({ type: "ADD_SET" });
@@ -66,12 +58,14 @@ const Exercise = ({
         dispatch({ type: "DELETE_SET", payload: id });
     };
 
-    const handleOpenSelector = () => {
-        setOpenExerciseSelector(true)
-    };
-
     const handleSetsRepsChange = (sets, reps) => {
         dispatch({ type: "UPDATE_PROGRAMME_SETS", payload: { sets, reps } });
+    };
+
+    const handleExerciseNameChange = (newName) => {
+        setExerciseName(newName);
+        dispatch({ type: "UPDATE_EXERCISE_NAME", payload: newName });
+        setOpenExerciseSelector(false)
     };
 
     const ExerciseSelectorWithActions = ({ exerciseNotYetChosen}) => {
@@ -104,51 +98,21 @@ const Exercise = ({
                 handleOpenSelector={handleOpenSelector}
             />
         );
-    } else {
+    };
+    
+    if (!programme) {
         return (
-            <Card sx={{ mt: 2, backgroundColor: 'grey.200' }}>
-                <CardContent>
-                    <Grid container spacing={1} alignItems="center" paddingBottom={1}>
-                        <Grid item md={3}>
-                            <Button onClick={() => setOpenExerciseSelector(true)}>
-                                <Typography variant="h6" fontSize={16}>{state.exerciseName}</Typography>
-                            </Button>
-                            <ExerciseSelectorWithActions/>
-                        </Grid>
-                    </Grid>
-                    {sets && sets.map((set, i) => (
-                        <SetContextProvider key={set.id}>
-                            <SetComponent 
-                                set={set} 
-                                onSetChange={(updatedSet) => handleSetChange(updatedSet, set.id)}
-                                onSetDelete={() => handleSetDelete(set.id)}
-                                initialSetData={initialExerciseData && initialExerciseData.sets[i]}
-                            />
-                        </SetContextProvider>
-                    ))}
-                    <Grid container spacing={1} marginTop={0} alignItems="center">
-                        <Grid item>
-                            <Button variant="contained" onClick={addSet}>Add Set</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button 
-                                variant="contained" 
-                                color="error" 
-                                onClick={handleDeleteExercise}
-                                disabled={workoutState.exercises.length <= 1}
-                                sx={{ justifyContent: "space-between" }}
-                                aria-label="Delete Exercise"
-                                title="Click to remove this exercise"
-                            >
-                                <RemoveCircleIcon sx={{ mr: 1 }}/>DELETE EXERCISE
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <ConsoleLogButton print={state} info="exercise"/>
-                        </Grid>
-                    </Grid>  
-                </CardContent>
-            </Card>
+            <WorkoutExerciseCard
+                exerciseState={state}
+                workoutState={workoutState}
+                initialExerciseData={initialExerciseData}
+                exerciseSelector={<ExerciseSelectorWithActions/>}
+                setOpenExerciseSelector={setOpenExerciseSelector}
+                handleDeleteExercise={handleDeleteExercise}
+                handleSetChange={handleSetChange}
+                handleSetDelete={handleSetDelete}
+                addSet={addSet}
+            />
         );
     };
 };
