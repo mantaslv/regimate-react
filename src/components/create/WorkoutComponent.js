c7b93dcc828b776097e6b2ffa4bee08d91dc0bf0
mantaslv <mantasvolkas@gmail.com>
2023-10-24 16:46:36 +0100

    Put logic back in to workout comp for programme create

--- src/components/create/WorkoutComponent.js
+++ src/components/create/WorkoutComponent.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useWorkoutContext } from "../../hooks/useWorkoutContext";
 import { ProgrammeSplitCard } from "./programme/ProgrammeSplitCard";
 import { WorkoutCard } from "./workout/WorkoutCard";
@@ -11,6 +11,14 @@ const WorkoutComponent = ({
     onWorkoutChange = () => {} // for testing purposes
 }) => {
     const { state, dispatch } = useWorkoutContext();
+    const [workoutName, setWorkoutName] = useState("");
+
+    useEffect(() => {
+        if (programme && initialWorkoutData) {
+            setWorkoutName(initialWorkoutData.workoutName);
+            dispatch({ type: "SET_WORKOUT", payload: initialWorkoutData });
+        };
+    }, []);
 
     const handleDeleteWorkout = () => {
         onWorkoutDelete();
@@ -28,14 +36,22 @@ const WorkoutComponent = ({
         dispatch({ type: "UPDATE_EXERCISE", payload: { id, changes: updatedExercise } });
     };
 
+    const handleWorkoutNameChange = (event) => {
+        const newName = event.target.value;
+        setWorkoutName(newName);
+        dispatch({ type: "UPDATE_WORKOUT_NAME", payload: newName });
+    };
+
     useEffect(() => onWorkoutChange(state), [state]); // for testing purposes
 
     if (programme) {
         return (
             <ProgrammeSplitCard
                 workoutState={state}
+                workoutName={workoutName}
                 exerciseList={exerciseList}
                 initialWorkoutData={initialWorkoutData}
+                handleWorkoutNameChange={handleWorkoutNameChange}
                 handleExerciseChange={handleExerciseChange}
                 handleExerciseDelete={handleExerciseDelete}
                 handleDeleteWorkout={handleDeleteWorkout}
