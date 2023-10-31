b3b8638af269da878334e8332d0af3eaf83fa486
mantaslv <mantasvolkas@gmail.com>
2023-10-31 16:38:24 +0000

    Programme data saved to context

--- src/pages/ProgrammeEditor.js
+++ src/pages/ProgrammeEditor.js
@@ -1,11 +1,23 @@
 import { useLocation } from "react-router-dom";
+import { useProgrammeContext } from "../hooks/useProgrammeContext";
+import { useEffect } from "react";
 
 const ProgrammeEditor = () => {
+    const { state, dispatch } = useProgrammeContext();
+
     const location = useLocation();
     const locationState = location.state || {};
     const programmeDataFromState = locationState.programmeData || null;
 
-    console.log(programmeDataFromState);
+    useEffect(() => {
+        if (programmeDataFromState) {
+            dispatch({ type: "SET_PROGRAMME", payload: programmeDataFromState });
+        };
+    }, [programmeDataFromState])
+
+    useEffect(() => {
+        console.log(state);
+    }, [state])
 };
 
 export default ProgrammeEditor;
