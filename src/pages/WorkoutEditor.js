c896f85961815c44cbff4010ae6c9e2de40d6c81
mantaslv <mantasvolkas@gmail.com>
2023-11-14 12:51:42 +0000

    Displays exercise names

--- src/pages/WorkoutEditor.js
+++ src/pages/WorkoutEditor.js
@@ -1,6 +1,51 @@
+import { useEffect, useState } from "react";
+import { useWorkoutContext } from "../hooks/useWorkoutContext"
+import { useLocation } from "react-router-dom";
+import { Box } from "@mui/material";
+import EditTrainingToolbar from "../components/create/EditTrainingToolbar";
+import Exercise from "../components/create/ExerciseComponent";
+
 const WorkoutEditor = () => {
+    const { state, dispatch } = useWorkoutContext();
+    const [initialLoadComplete, setInitialLoadComplete] = useState(false);
+    const [workoutName, setWorkoutName] = useState("Untitled Workout");
+    const [workoutData, setWorkoutData] = useState(null);
+
+    const location = useLocation();
+    const locationState = location.state || {};
+    const initialWorkoutData = locationState.workoutData || null;
+
+    const handleWorkoutNameChange = (event) => {
+        setWorkoutName(event.target.value);
+        dispatch({ type: "UPDATE_WORKOUT_NAME", payload: event.target.value });
+    };
+
+    useEffect(() => {
+        if (!initialLoadComplete && initialWorkoutData) {
+            dispatch({ type: "SET_WORKOUT", payload: initialWorkoutData });
+            setWorkoutName(initialWorkoutData.workoutName);
+            setWorkoutData(initialWorkoutData);
+            setInitialLoadComplete(true);
+        } else {
+            setWorkoutData(state);
+            console.log(state);
+        };
+    }, [initialWorkoutData, initialLoadComplete, state]);
+
     return (
-        <h1>hello</h1>
+        <Box>
+            <EditTrainingToolbar
+                nameInputValue={workoutName}
+                handleNameInputChange={handleWorkoutNameChange}
+                trainingData={workoutData}
+                isWorkout
+            />
+            <Box sx={{ my: '105px' }}>
+                {state.exercises.map((exercise, i) => (
+                    <h1>{exercise.exerciseName}</h1>
+                ))}
+            </Box>
+        </Box>
     )
 }
 export default WorkoutEditor
