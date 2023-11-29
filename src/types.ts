eeab223199181dd37b8a4aebc90ae3cf3842adbd
mantaslv <mantasvolkas@gmail.com>
2023-11-29 11:54:09 +0000

    Created ts types file and added training types

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface ProgrammeType {
+    _id: string;
+    user_id: string;
+    programmeName: string;
+    workouts: WorkoutType[];
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+    exerciseList: unknown;
+}
+  
+export interface WorkoutType {
+    id: string;
+    _id: string;
+    workoutName: string;
+    createdAt: string;
+    updatedAt: string;
+    exercises: ExerciseType[];
+}
+  
+export interface ExerciseType {
+    id: string;
+    _id: string;
+    exerciseName: string;
+    sets: SetType[];
+}
+
+export interface SetType {
+    id: string;
+    _id: string;
+    reps: string;
+    weight: string;
+}
