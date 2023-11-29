d055417aaafe0ea32e114ca86f061272e82ed264
mantaslv <mantasvolkas@gmail.com>
2023-11-29 16:13:44 +0000

    Removed create workout from reducer

--- src/context/workoutsContext.js
+++ src/context/workoutsContext.js
@@ -6,8 +6,6 @@ export const workoutsReducer = (state, action) => {
 	switch (action.type) {
 	case "SET_TRAINING_DATA":
 		return { workouts: action.payload };
-	case "CREATE_WORKOUT":
-		return { workouts: [action.payload, ...state.workouts] };
 	case "DELETE_WORKOUT":
 		return { workouts: state.workouts.filter((w) => w._id !== action.payload._id) };
 	default: 
