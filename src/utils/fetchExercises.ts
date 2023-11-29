274f9dda2449000f4bd6b099d6016082d89bd265
mantaslv <mantasvolkas@gmail.com>
2023-11-29 11:32:07 +0000

    Converted fetchExercises to ts

--- src/utils/fetchExercises.ts
+++ src/utils/fetchExercises.ts
@@ -1,12 +1,12 @@
 const fetchExercises = async () => {
 	try {
-		const res = await fetch(process.env.REACT_APP_API_URL + "/api/exercise-directory");
+		const res = await fetch(`${process.env.REACT_APP_API_URL}/api/exercise-directory`);
 		const json = await res.json();
     
 		if (res.ok) {
 			return json;
 		} else {
-			throw new Error("Error fetching exercises: ", json);
+			throw new Error(`Error fetching exercise list: ${JSON.stringify(json)}`);
 		}
 	} catch (error) {
 		console.error("Error fetching exercises:", error);
