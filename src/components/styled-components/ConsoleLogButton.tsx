90a2c07c1c4f49620a65d6fdea0b94e528ee43aa
mantaslv <mantasvolkas@gmail.com>
2023-12-03 11:36:33 +0000

    ConsoleLogButton to tsx

---
+++ src/components/styled-components/ConsoleLogButton.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import { Button, ButtonProps } from "@mui/material";
+import TerminalIcon from "@mui/icons-material/Terminal";
+
+interface ConsoleLogButtonProps {
+	print: object;
+	info: string;
+	sx?: ButtonProps["sx"];
+	size?: ButtonProps["size"];
+	variant?: ButtonProps["variant"];
+}
+
+const ConsoleLogButton: FC<ConsoleLogButtonProps> = ({ print, info, sx, size, variant="contained" }) => {
+	if (process.env.NODE_ENV === "development") {
+		return (
+			<Button
+				variant={variant} 
+				onClick={() => console.log(print)}
+				title={`Click to console log this ${info}`}
+				sx={sx}
+				size={size}
+			>
+				<TerminalIcon/>
+			</Button>
+		);
+	} else {
+		return null;
+	}
+};
+
+export default ConsoleLogButton;
