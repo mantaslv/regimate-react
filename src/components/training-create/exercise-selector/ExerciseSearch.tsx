bcb44f3ce11022948d9de7a9b30a586fb4a5f349
mantaslv <mantasvolkas@gmail.com>
2024-03-07 10:52:59 +0000

    Refactored exercise search

---
+++ src/components/training-create/exercise-selector/ExerciseSearch.tsx
@@ -0,0 +1,61 @@
+import React, { FC, useState } from "react";
+import { Input, Box } from "@mui/material";
+import { ExerciseListObjectType } from "../../../types";
+
+interface ExerciseSearchProps {
+    exerciseList: ExerciseListObjectType[];
+    onFilter: (filteredExercises: ExerciseListObjectType[]) => void;
+}
+
+const ExerciseSearch: FC<ExerciseSearchProps> = ({ exerciseList, onFilter }) => {
+	const [searchTerm, setSearchTerm] = useState<string>("");
+
+	const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const term = event.target.value;
+		setSearchTerm(term);
+
+		const exactMatchFilteredExercises = exerciseList?.filter((exercise: ExerciseListObjectType) => 
+			exercise.name.toLowerCase().includes(searchTerm.toLowerCase())
+		) ?? [];
+
+		const splitMatchFilteredExercises = exerciseList?.filter(exercise => 
+			!exactMatchFilteredExercises.includes(exercise) &&
+			searchTerm.toLowerCase().split(" ").every(word => 
+				exercise.name.toLowerCase().split(" ").includes(word)
+			)
+		) ?? [];
+
+		const partialMatchFilteredExercises = exerciseList?.filter(exercise => 
+			!exactMatchFilteredExercises.includes(exercise) &&
+        !splitMatchFilteredExercises.includes(exercise) &&
+			searchTerm.toLowerCase().split(" ").some(word =>
+				exercise.name.toLowerCase().split(" ").includes(word)
+			)
+		) ?? [];
+    
+		const filteredExercises = [
+			...exactMatchFilteredExercises, 
+			...splitMatchFilteredExercises, 
+			...partialMatchFilteredExercises
+		];
+		onFilter(filteredExercises);
+	};
+
+	return (
+		<Box sx={{ display: "flex", justifyContent: "center" }}>
+			<Input 
+				placeholder="Search" 
+				disableUnderline
+				value={searchTerm} 
+				onChange={handleSearchChange} 
+				sx={{ 
+					p: 1,
+					width: "100%",
+					borderBottom: "1px solid #dcdcdc", 
+				}}
+			/>
+		</Box>
+	);
+};
+
+export default ExerciseSearch;
--- src/components/training-create/exercise-selector/ExerciseSelectorNew.tsx
+++ src/components/training-create/exercise-selector/ExerciseSelectorNew.tsx
@@ -1,75 +1,30 @@
-import { Box,  Input, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React, { FC, useEffect, useState } from "react";
 import { useProgrammeContext } from "../../../hooks/useProgrammeContext";
 import { useWorkoutContext } from "../../../hooks/useWorkoutContext";
 import { ExerciseListObjectType } from "../../../types";
 import ExerciseOptionCard from "./ExerciseOption";
+import ExerciseSearch from "./ExerciseSearch";
 
 interface ExerciseSelectorProps {
-	// isExerciseSelectorOpen: boolean;
-	// onOpenDialog: (value: boolean) => void;
-	// onExerciseSelection: (exerciseName: string) => void;
 	inWorkout?: boolean;
 }
 
 const ExerciseSelector: FC<ExerciseSelectorProps> = ({ 
-	// isExerciseSelectorOpen, 
-	// onOpenDialog, 
-	// onExerciseSelection, 
 	inWorkout=false
 }) => {
-// const ExerciseSelector = () => {
 	const { state } = inWorkout ? useWorkoutContext() : useProgrammeContext();
-	// const { state } = useProgrammeContext();
-	const [searchTerm, setSearchTerm] = useState<string>("");
-	const [exerciseList, setExerciseList] = useState<ExerciseListObjectType[]>([]);
+	const [filteredExercises, setFilteredExercises] = useState<ExerciseListObjectType[]>([]);
 
-	useEffect(() => setExerciseList(state.exerciseList), [state]);
+	useEffect(() => setFilteredExercises(state.exerciseList), [state]);
 
-	const exactMatchFilteredExercises = exerciseList?.filter((exercise: ExerciseListObjectType) => 
-		exercise.name.toLowerCase().includes(searchTerm.toLowerCase())
-	) ?? [];
-
-	const splitMatchFilteredExercises = exerciseList?.filter(exercise => 
-		!exactMatchFilteredExercises.includes(exercise) &&
-			searchTerm.toLowerCase().split(" ").every(word => 
-				exercise.name.toLowerCase().split(" ").includes(word)
-			)
-	) ?? [];
-
-	const partialMatchFilteredExercises = exerciseList?.filter(exercise => 
-		!exactMatchFilteredExercises.includes(exercise) &&
-        !splitMatchFilteredExercises.includes(exercise) &&
-			searchTerm.toLowerCase().split(" ").some(word =>
-				exercise.name.toLowerCase().split(" ").includes(word)
-			)
-	) ?? [];
-    
-	const filteredExercises = [
-		...exactMatchFilteredExercises, 
-		...splitMatchFilteredExercises, 
-		...partialMatchFilteredExercises
-	];
-
-	const handleSearchChange = (event: { target: { value: string }; }) => {
-		setSearchTerm(event.target.value);
+	const handleFilteredExercises = (exercises: ExerciseListObjectType[]) => {
+		setFilteredExercises(exercises);
 	};
 
 	return(
 		<Box>
-			<Box sx={{ display: "flex", justifyContent: "center" }}>
-				<Input 
-					placeholder="Search" 
-					disableUnderline
-					value={searchTerm} 
-					onChange={handleSearchChange} 
-					sx={{ 
-						p: 1,
-						width: "100%",
-						borderBottom: "1px solid #dcdcdc", 
-					}}
-				/>
-			</Box>
+			<ExerciseSearch exerciseList={state.exerciseList} onFilter={handleFilteredExercises} />
 			<Box sx={{ 
 				maxHeight: "calc(100vh - 120px)",
 				overflowY: "auto",
