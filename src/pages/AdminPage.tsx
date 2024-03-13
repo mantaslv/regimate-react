721540155acb36b873502d40ce2e81ae3e95d860
mantaslv <mantasvolkas@gmail.com>
2024-03-13 11:32:03 +0000

    Created exercise list table container in admin page

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,10 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Paper, Table, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import React from "react";
 
 const AdminPage = () => {
-	return (
-		<Box>
 
+
+	return (
+		<Box sx={{ mt: "55px", display: "flex", justifyContent: "center" }}>
+			<Paper sx={{ maxWidth: "1000px", overflow: "hidden" }}>
+				<TableContainer sx={{ maxHeight: 440 }}>
+					<Table stickyHeader aria-label="sticky table">
+						<TableHead>
+							<TableRow>
+								<TableCell>
+									Hello World!
+								</TableCell>
+							</TableRow>
+						</TableHead>
+					</Table>
+				</TableContainer>
+			</Paper>
 		</Box>
 	);
 };
