ecd02c43956ec6b45b9f82f59b4124657ce12241
mantaslv <mantasvolkas@gmail.com>
2024-03-14 18:14:32 +0000

    Fixed toTitleCase to include hyphens

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,6 @@
 export const toTitleCase = (str: string) => {
 	return str.replace(
-		/\w\S*/g, 
-		(txt: string) => txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase()
+		/\b[a-z]/gi,
+		(char) => char.toUpperCase()
 	);
 };
