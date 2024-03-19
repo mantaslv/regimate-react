b115e4a970ac23d737aa2cec7cffcb760e2f3be9
mantaslv <mantasvolkas@gmail.com>
2024-03-19 13:55:55 +0000

    Calendar shows current month

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,10 +1,13 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { format } from "date-fns";
 
 const CalendarPage = () => {
+	const [currentMonth, setCurrentMonth] = useState(new Date());
+
 	return (
 		<Box sx={{ mt: "45px"}}>
-			This will be the calendar page!
+			{format(currentMonth, "MMMM, yyyy")}
 		</Box>
 	);
 };
